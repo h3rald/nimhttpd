91ac711138d852c71ab2dbc0f6bd72dc87e6fc36
h3rald <h3rald@h3rald.com>
2018-10-21 14:14:50 +0200

    Added support for Nimble v0.9.0

--- src/nimhttpdpkg/config.nim
+++ src/nimhttpdpkg/config.nim
@@ -1,5 +1,5 @@
 const
   pkgTitle*       = "NimHTTPd"
-  pkgVersion*     = "1.0.5"
+  pkgVersion*     = "1.0.6"
   pkgAuthor*      = "Fabio Cevasco"
   pkgDescription* = "A tiny static file web server."
