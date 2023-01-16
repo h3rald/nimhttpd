7e0342582a912e00bddc03aaaa796b106c5caaeb
Michael Adams <unquietwiki@gmail.com>
2023-01-15 18:23:52 -0800

    Revise app to be dual-stack IPv4+IPv6

--- src/nimhttpdpkg/config.nim
+++ src/nimhttpdpkg/config.nim
@@ -1,5 +1,5 @@
 const
   pkgTitle*       = "NimHTTPd"
-  pkgVersion*     = "1.4.1"
+  pkgVersion*     = "1.5.0"
   pkgAuthor*      = "Fabio Cevasco & Michael Adams"
   pkgDescription* = "A tiny static file web server. IPv4 & IPv6 supported!"
