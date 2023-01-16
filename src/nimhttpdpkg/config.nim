ee386274a8c0491285ac4931fb36175267c42580
Michael Adams <unquietwiki@gmail.com>
2023-01-15 17:56:03 -0800

    tweaks

--- src/nimhttpdpkg/config.nim
+++ src/nimhttpdpkg/config.nim
@@ -1,5 +1,5 @@
 const
   pkgTitle*       = "NimHTTPd"
-  pkgVersion*     = "1.4.0"
+  pkgVersion*     = "1.4.1"
   pkgAuthor*      = "Fabio Cevasco & Michael Adams"
   pkgDescription* = "A tiny static file web server. IPv4 & IPv6 supported!"
