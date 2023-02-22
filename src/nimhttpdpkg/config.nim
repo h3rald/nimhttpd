89e5dc03fd77e545261bd812cded14c8b2b2cf86
h3rald <h3rald@h3rald.com>
2023-02-22 14:29:37 +0000

    Updated version.

--- src/nimhttpdpkg/config.nim
+++ src/nimhttpdpkg/config.nim
@@ -1,5 +1,5 @@
 const
   pkgTitle*       = "NimHTTPd"
-  pkgVersion*     = "1.5.0"
+  pkgVersion*     = "1.5.1"
   pkgAuthor*      = "Fabio Cevasco & Michael Adams"
   pkgDescription* = "A tiny static file web server. IPv4 & IPv6 supported!"
